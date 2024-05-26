d1f49fd81ae427faf4c726a18bcc3388b84850a1
Mostakimul Karim <mostakimulkarim@gmail.com>
2024-05-26 17:07:07 -0400

    [feat]: configureing axios

---
+++ src/helpers/axios/axiosInstance.ts
@@ -0,0 +1,59 @@
+import { authKey } from '@/constant/authKey';
+import setAccessToken from '@/services/actions/setAccessToken';
+import { getNewAccessToken } from '@/services/auth.services';
+import { TGenericErrorResponse, TResponseSuccessType } from '@/types';
+import { getFromLocalStorage, setToLocalStorage } from '@/utils/local-storage';
+import axios from 'axios';
+
+const instance = axios.create();
+instance.defaults.headers.post['Content-Type'] = 'application/json';
+instance.defaults.headers['Accept'] = 'application/json';
+instance.defaults.timeout = 60000;
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  function (config) {
+    const accessToken = getFromLocalStorage(authKey);
+
+    if (accessToken) {
+      config.headers.Authorization = accessToken;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  //@ts-ignore
+  function (response) {
+    const responseObject: TResponseSuccessType = {
+      data: response?.data?.data,
+      meta: response?.data?.meta,
+    };
+    return responseObject;
+  },
+  async function (error) {
+    const config = error.config;
+    if (error?.response?.status === 500 && !config.sent) {
+      config.sent = true;
+      const response = await getNewAccessToken();
+      const accessToken = response?.data?.accessToken;
+      config.headers['Authorization'] = accessToken;
+      setToLocalStorage(authKey, accessToken);
+      setAccessToken(accessToken);
+      return instance(config);
+    } else {
+      const responseObject: TGenericErrorResponse = {
+        statusCode: error?.response?.data?.statusCode || 500,
+        message: error?.response?.data?.message || 'Something went wrong!!!',
+        errorMessages: error?.response?.data?.message,
+      };
+      return responseObject;
+    }
+  },
+);
+
+export { instance };
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,80 +1,9 @@
-import {
-  BaseQueryApi,
-  BaseQueryFn,
-  DefinitionType,
-  FetchArgs,
-  createApi,
-  fetchBaseQuery,
-} from '@reduxjs/toolkit/query/react';
-import { toast } from 'sonner';
-import { logout, setUser } from '../features/authSlice';
-import { RootState } from '../store';
-
-interface ErrorData {
-  message: string;
-}
-const baseQuery = fetchBaseQuery({
-  baseUrl: process.env.APP_API_URL,
-  credentials: 'include',
-  prepareHeaders: (headers, { getState }) => {
-    const token = (getState() as RootState).auth.token;
-
-    if (token) {
-      headers.set('authorization', `${token}`);
-    }
-
-    return headers;
-  },
-});
-
-const baseQueryWithRefreshToken: BaseQueryFn<
-  FetchArgs,
-  BaseQueryApi,
-  DefinitionType
-> = async (args, api, extraOptions): Promise<any> => {
-  let result = await baseQuery(args, api, extraOptions);
-
-  if (result?.error?.status === 404) {
-    const errorData = result.error.data as ErrorData;
-    toast.error(errorData.message);
-  }
-  if (result?.error?.status === 403) {
-    const errorData = result.error.data as ErrorData;
-    toast.error(errorData.message);
-  }
-  if (result?.error?.status === 401) {
-    //* Send Refresh
-    console.log('Sending refresh token');
-
-    const res = await fetch(`${process.env.APP_API_URL}/refresh-token`, {
-      method: 'POST',
-      credentials: 'include',
-    });
-
-    const data = await res.json();
-
-    if (data?.data?.accessToken) {
-      const user = (api.getState() as RootState).auth.user;
-
-      api.dispatch(
-        setUser({
-          user,
-          token: data.data.accessToken,
-        }),
-      );
-
-      result = await baseQuery(args, api, extraOptions);
-    } else {
-      api.dispatch(logout());
-    }
-  }
-
-  return result;
-};
+import { axiosBaseQuery } from '@/helpers/axios/axiosBaseQuery';
+import { createApi } from '@reduxjs/toolkit/query/react';
 
 export const baseApi = createApi({
-  reducerPath: 'baseApi',
-  baseQuery: baseQueryWithRefreshToken,
+  reducerPath: 'api',
+  baseQuery: axiosBaseQuery({ baseUrl: process.env.APP_API_URL as string }),
   tagTypes: [],
   endpoints: () => ({}),
 });
