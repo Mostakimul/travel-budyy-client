cb80d829d2c5eb8a5bd7152f73ab42f9666a38a2
Mostakimul Karim <mostakimulkarim@gmail.com>
2024-06-05 00:54:19 -0400

    [feat]: super admin

--- src/app/(dashbordLayout)/components/tables/userTable.tsx
+++ src/app/(dashbordLayout)/components/tables/userTable.tsx
@@ -17,10 +17,6 @@ const UserTable = ({
   handleChangeRole,
 }: TUserTableProps) => {
   const user = getUserInfo();
-  // const router = useRouter()
-  // const handleEdit = () => {
-  //   router
-  // }
 
   return (
     <tr className="hover hover:text-gray-900">
@@ -28,49 +24,51 @@ const UserTable = ({
       <td>{row.email}</td>
       <td>{row.role}</td>
 
-      <td className="space-x-3">
-        {row.status === 'ACTIVE' ? (
-          <>
-            <Link
-              href={`/dashboard/admin/users/${row.id}`}
-              className="btn btn-sm btn-outline btn-primary"
-            >
-              Edit
-            </Link>
+      {row.role !== 'SUPER_ADMIN' && (
+        <td className="space-x-3">
+          {row.status === 'ACTIVE' ? (
+            <>
+              <Link
+                href={`/dashboard/admin/users/${row.id}`}
+                className="btn btn-sm btn-outline btn-primary"
+              >
+                Edit
+              </Link>
 
-            {user?.email !== row.email && (
-              <>
-                <button
-                  className="btn btn-sm btn-outline btn-accent"
-                  onClick={() => {
-                    handleChangeRole &&
-                      handleChangeRole({ email: row.email, role: row.role });
-                  }}
-                >
-                  Change Role
-                </button>
-                <button
-                  className="btn btn-sm btn-outline btn-info"
-                  onClick={() => {
-                    handleBlock && handleBlock(row.email);
-                  }}
-                >
-                  Block
-                </button>
-              </>
-            )}
-          </>
-        ) : (
-          <button
-            className="btn btn-sm btn-outline btn-info"
-            onClick={() => {
-              handleUnblock && handleUnblock(row.email);
-            }}
-          >
-            Unblock
-          </button>
-        )}
-      </td>
+              {user?.email !== row.email && (
+                <>
+                  <button
+                    className="btn btn-sm btn-outline btn-accent"
+                    onClick={() => {
+                      handleChangeRole &&
+                        handleChangeRole({ email: row.email, role: row.role });
+                    }}
+                  >
+                    Change Role
+                  </button>
+                  <button
+                    className="btn btn-sm btn-outline btn-info"
+                    onClick={() => {
+                      handleBlock && handleBlock(row.email);
+                    }}
+                  >
+                    Block
+                  </button>
+                </>
+              )}
+            </>
+          ) : (
+            <button
+              className="btn btn-sm btn-outline btn-info"
+              onClick={() => {
+                handleUnblock && handleUnblock(row.email);
+              }}
+            >
+              Unblock
+            </button>
+          )}
+        </td>
+      )}
     </tr>
   );
 };
