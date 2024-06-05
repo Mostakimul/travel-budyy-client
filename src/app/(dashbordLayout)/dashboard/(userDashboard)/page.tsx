71e93b908180ad738e7334cb72ebb0591ccf5abc
Mostakimul Karim <mostakimulkarim@gmail.com>
2024-06-05 07:38:55 -0400

    feat: home page redirect

--- src/app/(dashbordLayout)/dashboard/(userDashboard)/page.tsx
+++ src/app/(dashbordLayout)/dashboard/(userDashboard)/page.tsx
@@ -1,5 +1,15 @@
+import Link from 'next/link';
+
 const UserDashboard = () => {
-  return <div>UserDashboard</div>;
+  return (
+    <div className="text-base-200">
+      <p>UserDashboard</p>
+
+      <Link href={'/'} className="btn btn-sm btn-info mx-auto w-full mt-5">
+        Home Page
+      </Link>
+    </div>
+  );
 };
 
 export default UserDashboard;
--- src/app/(dashbordLayout)/dashboard/admin/page.tsx
+++ src/app/(dashbordLayout)/dashboard/admin/page.tsx
@@ -1,5 +1,15 @@
+import Link from 'next/link';
+
 const AdminHomePage = () => {
-  return <div>AdminHomePage</div>;
+  return (
+    <div className="text-base-200">
+      <p>AdminDashboard</p>
+
+      <Link href={'/'} className="btn btn-sm btn-info mx-auto w-full mt-5">
+        Home Page
+      </Link>
+    </div>
+  );
 };
 
 export default AdminHomePage;
